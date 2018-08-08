3855471dc8eb6d1e77ab03db69058d889bbf7190
Andy Davies <andy@mojotech.com>
2018-08-07 21:01:54 -0400

    font files and starting to refactor base templates and styles

---
+++ components/CityLockup.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="city">
+    <div class="city-icon" />
+    <nuxt-link to="/location" class="city-title">
+          Providence
+    </nuxt-link>
+  </div>
+</template>
+
+<style>
+.city-title {
+  font-size: 2.25rem;
+  color: var(--offBlack);
+  text-decoration: none;
+  &:hover {
+    color: var(--gold);
+  }
+}
+</style>
+
+
